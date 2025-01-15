a110fd1cd3c8b46b75db5dce76a98c318ea711d4
Sean La <seanla1996@gmail.com>
2025-01-15 15:37:44 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_package",
+    name="job_posting_analyzer",
     version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
         # Add your package dependencies here
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A short description of your package",
+    author="Sean La",
+    author_email="sean.la.msc@icloud.com",
+    description="Analyze job posts with LLMs",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_package",
+    url="https://github.com/sean-la/job-posting-analyzer",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
