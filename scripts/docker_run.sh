758bab1f27d91167794688a56e073be90fc40b9d
Sean La <seanla1996@gmail.com>
2025-01-20 14:50:37 -0800

    update docker run script to accept any config file path

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -3,8 +3,8 @@
 # Get command line arguments
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    --data)
-      DATA="$2"
+    --config)
+      CONFIG="$2"
       shift 2
       ;;
     *)
@@ -15,21 +15,27 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Check if required arguments are provided
-if [[ -z "$DATA" ]]; then
+if [[ -z "$CONFIG" ]]; then
   echo "Error: --data argument is required"
   exit 1
 fi
 
+# Get the absolute path of the file
+ABS_PATH=$(readlink -f "$CONFIG")
+
+# Extract the directory part from the path
+DIR="${ABS_PATH%/*}"
+FILENAME=$(basename $CONFIG)
+
 # Build the docker run command
 docker run \
-  -v "$DATA":/app/data\
+  -v "$DIR":/app/data\
   -v /var/tmp/jobs:/var/tmp/jobs\
   -v $HOME/.config/gcloud:/root/.config/gcloud \
   -e GOOGLE_API_KEY=$GOOGLE_API_KEY \
   -e GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/application_default_credentials.json \
   -e GOOGLE_CLOUD_PROJECT="$GOOGLE_CLOUD_PROJECT" \
   job-posting-analyzer:dev \
-  --config "./data/config.json" \
-  --resume "./data/resume.pdf" \
+  --config /app/data/$FILENAME
   --loglevel DEBUG \
   --ignore_job_id
